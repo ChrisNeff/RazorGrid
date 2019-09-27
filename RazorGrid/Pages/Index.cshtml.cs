71f9a993286078d660ec7b61309da5172c767194
Chris Neff <christopher.neff@live.com>
2019-09-26 21:33:03 -0400

    [Initial-Commit] Working sample with .NET Core 3.0

---
+++ RazorGrid/Pages/Index.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace RazorGrid.Pages
+{
+    /// <summary>
+    /// http://mvc6-grid.azurewebsites.net/Home/Installation
+    /// </summary>
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public IQueryable<Person> People { get; set; }
+
+        public void OnGet()
+        {
+            People = new[]
+            {
+                new Person
+                {
+                    Name = "ABC",
+                    SomeNumber = 3.1
+                },
+                new Person
+                {
+                    Name = "XYZ",
+                    SomeNumber = 7.5
+                }
+            }
+            .AsQueryable();
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public double SomeNumber { get; set; }
+        public DateTime Today => DateTime.UtcNow.ToLocalTime().Date;
+    }
+}
